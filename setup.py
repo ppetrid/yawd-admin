3fa128f0c22d5b8bfd9a6380b2d7c2df4024744d
ppetrid <ppetrid@yawd.eu>
2012-12-04 22:31:52 +0200

    fix distutils version

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-import yawdadmin
 
 setup(
       name='yawd-admin',
       url='http://yawd.eu/open-source-projects/yawd-admin/',
-      version = yawdadmin.__version__,
+      version = '0.5.0',
       description='An administration website for Django',
       author='yawd',
       author_email='info@yawd.eu',
