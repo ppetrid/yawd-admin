e77f98be8d27d878cfd5a12ef32cd8e445e6be15
mwolff44 <mathiaswolff@mac.com>
2016-05-31 12:14:25 +0200

    django 1.9 warning - appl_label not empty

--- yawdadmin/models.py
+++ yawdadmin/models.py
@@ -12,6 +12,7 @@ class AppOption(models.Model):
     class Meta:
         unique_together = ('optionset_label', 'name')
         ordering = ['optionset_label', 'lang_dependant']
+        app_label = 'AppOption'
 
     def __unicode__(self):
         return u'%s.%s' % (self.optionset_label, self.name)
